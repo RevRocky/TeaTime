72dde40310532d76a5c6866590c0eefa01710275
Rocky Petkov <mafiastewie666@gmail.com>
2017-07-22 11:18:41 -0600

    Increased support for a phone in land scape mode as well as the ability to sort through all of the teas in the system based on different parameters.

--- app/src/main/java/rocky/teatime/activities/DatabaseActivity.java
+++ app/src/main/java/rocky/teatime/activities/DatabaseActivity.java
@@ -18,7 +18,10 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import rocky.teatime.R;
 import rocky.teatime.database.DataSource;
@@ -33,15 +36,29 @@ public class DatabaseActivity extends AppCompatActivity {
     private final int GRID_WIDTH = 3;
 
     public final int NEW_TEA_REQUEST = 40; // A code given to the new tea request
+
+    // Some short integer codes which we can set to ensure the database is sorted how the user
+    // wishes it.
+    private static final byte SORT_BY_TYPE = 0;
+    private static final byte SORT_BY_AZ = 1;
+    private static final byte SORT_BY_ZA = 2;
+    private static final byte SORT_BY_NEW = 3;
+    private static final byte SORT_BY_OLD = 4;
+
+    private static final boolean CHANGED_DB_SORT = true;
+
     private DataSource dbHelper;
 
     private RecyclerView recyclerView;      // The overall recycler view object
     private DatabaseVisualiser visualiser;  // The specific visualier used to display things from the database
     private RecyclerView.LayoutManager layoutManager;
+    private byte sortType = SORT_BY_OLD;    // Default to order added to DB first.
+
 
     /**
      * Establishes the basics so that a grid based recycler view can be used to view the different
      * types of teas.
+     *
      * @param savedInstanceState Saved state of the last time the actiity was ran
      */
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +75,7 @@ public class DatabaseActivity extends AppCompatActivity {
 
         // Opening our database helper
         dbHelper = new DataSource(this);
-        updateTeaList();
+        updateTeaList(!CHANGED_DB_SORT);
 
         // Registering our recycler view for a context menu
         registerForContextMenu(recyclerView);
@@ -66,6 +83,7 @@ public class DatabaseActivity extends AppCompatActivity {
 
     /**
      * Fetches the Action Bar Menu
+     *
      * @param menu A menu class which we will inject the DBMenu
      * @return True if successful, false otherwise
      */
@@ -77,6 +95,7 @@ public class DatabaseActivity extends AppCompatActivity {
 
     /**
      * A central clearing house for selcting all menu options
+     *
      * @param item The menu item selected
      * @return True if the operation is successful
      */
@@ -94,6 +113,27 @@ public class DatabaseActivity extends AppCompatActivity {
             case R.id.submitFeedback:
                 launchEmailActivity();
                 return true;
+            case R.id.TypeSort:
+                sortType = SORT_BY_TYPE;
+                updateTeaList(CHANGED_DB_SORT);
+                return true;
+            case R.id.AtoZSort:
+                sortType = SORT_BY_AZ;
+                updateTeaList(CHANGED_DB_SORT);
+                return true;
+            case R.id.ZtoASort:
+                sortType = SORT_BY_ZA;
+                updateTeaList(CHANGED_DB_SORT);
+                return true;
+            case R.id.oldestFirst:
+                sortType = SORT_BY_OLD;
+                updateTeaList(CHANGED_DB_SORT);
+                return true;
+            case R.id.newestFirst:
+                sortType = SORT_BY_OLD;
+                updateTeaList(CHANGED_DB_SORT);
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -101,6 +141,7 @@ public class DatabaseActivity extends AppCompatActivity {
 
     /**
      * A central clearing house for all context menu options
+     *
      * @param item Menu item selected
      * @return True if operation is successful
      */
@@ -111,8 +152,7 @@ public class DatabaseActivity extends AppCompatActivity {
         // Attempt to retrieve position
         try {
             position = visualiser.getPosition();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             Log.d("ERROR WITH CONTEXT MENU", e.getLocalizedMessage(),
                     e);
             return super.onContextItemSelected(item);
@@ -157,6 +197,7 @@ public class DatabaseActivity extends AppCompatActivity {
     /**
      * Launches the tea brew timer. If two times are present for the tea, it asks the user which
      * steeping the tea is.
+     *
      * @param teaToBrew Tea we wish to brew!
      */
     private void launchBrewScreen(Tea teaToBrew) {
@@ -167,8 +208,7 @@ public class DatabaseActivity extends AppCompatActivity {
             // TODO Create alert so user can pick. For now defaulting to first time
             // for testing purposes
             timerIntent.putExtra(TimerActivity.START_KEY, teaToBrew.getBrewTime());
-        }
-        else {
+        } else {
             timerIntent.putExtra(TimerActivity.START_KEY, teaToBrew.getBrewTime());
         }
         startActivity(timerIntent);     // Start the timer activity!
@@ -176,33 +216,35 @@ public class DatabaseActivity extends AppCompatActivity {
 
     /**
      * Removes the specified tea from the database
+     *
      * @param teaToDelete Tea we wish to remove from the database
      */
     private void deleteTea(Tea teaToDelete) {
         teaToDelete.createTeaRemoveAlert(this);
 
         if (teaToDelete.getId() == Tea.TEA_REMOVED_ID_FLAG) {
-            updateTeaList();    // We only need to update the list if we've deleted something
+            updateTeaList(!CHANGED_DB_SORT);    // We only need to update the list if we've deleted something
         }
     }
 
     /**
      * A central clearing house for handling all results of dispatched activities
+     *
      * @param requestCode A code unique to the activity dispatched
-     * @param resultCode A code informing us of whether or not the result was successful
-     * @param data A payload containing information from the dispatch activity
+     * @param resultCode  A code informing us of whether or not the result was successful
+     * @param data        A payload containing information from the dispatch activity
      */
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
             case (NEW_TEA_REQUEST):
-                updateTeaList();    // We don't actually care to
+                updateTeaList(!CHANGED_DB_SORT);    // We don't actually care to
                 return;
             case (EditTeaActivity.EDIT_TEA_REQUEST):
-                updateTeaList();
+                updateTeaList(!CHANGED_DB_SORT);
                 return;
-            case (ViewTeaActivity.VIEW_TEA_REQUEST) :
+            case (ViewTeaActivity.VIEW_TEA_REQUEST):
                 if (resultCode == ViewTeaActivity.DATABASE_CHANGE_FLAG) {
-                    updateTeaList();    // Changes have happened so we want to update the display
+                    updateTeaList(!CHANGED_DB_SORT);    // Changes have happened so we want to update the display
                 }
                 return;
         }
@@ -211,22 +253,84 @@ public class DatabaseActivity extends AppCompatActivity {
     /**
      * Updates the list view with the latest information from the database. Should the database not
      * exist, it will catch the Null Pointer Error and... do nothing!
+     *
+     * @param changeSort A simple flag informing the method as to whether we should spend the time
+     *                   to sort the list returned from the data base.
      */
-    private void updateTeaList() {
+    private void updateTeaList(boolean changeSort) {
         // If the database does not exist a Null Pointer exception will be thrown. This is no good.
         try {
             dbHelper.open();
             ArrayList<Tea> teaList = dbHelper.getAllEntries();
 
-            //TODO check to see if user wants list or grid mode
+            if (changeSort) {
+                teaList = sortTeaList(teaList);
+            }
+
             // Here I'm making use of a custom database visualiser!
             visualiser = new GridVisualiser(teaList, this);
             recyclerView.setAdapter(visualiser);
             dbHelper.close();
 
-        }
-        catch (Exception | Error e) {
+        } catch (Exception | Error e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Sorts the supplied list of teas in a manner in accordance with the field "sortType"
+     * @param teaList Unsorted list of teas
+     * @return List of teas sorted by the parameter of the users choosing. This figure can be
+     * ascertained by looking at the field "sortType"
+     */
+    private ArrayList<Tea> sortTeaList(final ArrayList<Tea> teaList) {
+        switch (sortType) {
+            case (SORT_BY_TYPE):
+                Collections.sort(teaList, new Comparator<Tea> () {
+                    @Override
+                    public int compare(Tea teaOne, Tea teaTwo) {
+                        return  ((Integer)teaOne.getType().ordinal()).compareTo(teaTwo.getType().ordinal());
+                    }
+                });
+                return teaList;
+            case (SORT_BY_AZ):
+                Collections.sort(teaList, new Comparator<Tea>() {
+                    @Override
+                    public int compare(Tea teaOne, Tea teaTwo) {
+                        return teaOne.getName().compareTo(teaTwo.getName());
+                    }
+                });
+                return teaList;
+            case (SORT_BY_ZA):
+                Collections.sort(teaList, new Comparator<Tea>() {
+                    @Override
+                    public int compare(Tea teaOne, Tea teaTwo) {
+                        return teaOne.getName().compareTo(teaTwo.getName());
+                    }
+                });
+                Collections.reverse(teaList);
+                return teaList;
+            case (SORT_BY_NEW):
+                // Idea is that the ID is sequential and thus will track what is most recently added
+                // to the database
+                Collections.sort(teaList, new Comparator<Tea>() {
+                    @Override
+                    public int compare(Tea teaOne, Tea teaTwo) {
+                        return ((Long) teaOne.getId()).compareTo(teaTwo.getId());
+                    }
+                });
+                return teaList;
+            case (SORT_BY_OLD):
+                // Again ID is a perfect indicator of when something was added
+                Collections.sort(teaList, new Comparator<Tea>() {
+                    @Override
+                    public int compare(Tea teaOne, Tea teaTwo) {
+                        return ((Long) teaTwo.getId()).compareTo(teaTwo.getId());
+                    }
+                });
+                return teaList;
+            default:
+                return teaList;
         }
     }
 }
